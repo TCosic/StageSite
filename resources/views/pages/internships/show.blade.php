71ab03a8ae41be515503788a670e0452b26d4603
Falco <falco@gerritsjans.com>
2016-06-02 12:12:12 +0200

    Inhoud omgewisselt

--- resources/views/pages/internships/show.blade.php
+++ resources/views/pages/internships/show.blade.php
@@ -3,18 +3,5 @@
 @section('title', 'Internship details')
 
 @section('content')
-    <section>
-        <h2>Stageplekken</h2>
-        <article>
-            <header>
-                <h3>Bedrijfsnaam (<a href="#">website.nl</a>)</h3>
-                <p><span>Locatie </span>Straatnaam 69, Rotterdam</p>
-                <p><span>Telefoon </span>010 1234567</p>
-                <p><span>Opleiding </span>Mediaontwikkelaar</p>
-                <p><span>Leerweg </span>BBL/BOL</p>
-                <p><span>Tijd </span>400 uur</p>
-            </header>
-            <p>Beschrijving etc</p>
-        </article>
-    </section>
+    <h2>Sample Text</h2>
 @stop
