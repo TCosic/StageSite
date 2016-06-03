3c33a6b09ebd5044319b9e7738fa6ac3ca3d73dc
Falco <falco@gerritsjans.com>
2016-06-03 11:56:17 +0200

    Spaties

--- resources/views/pages/internships/show.blade.php
+++ resources/views/pages/internships/show.blade.php
@@ -8,11 +8,11 @@
         <header>
             <h3>{{$stage->contact->company->name}}</h3>
             <ul>
-                <li><span>Locatie: </span> {{$stage->contact->company->fullAddress() }}</li>
-                <li><span>Telefoon: </span> <a href="tel:{{$stage->contact->company->tel}}">{{$stage->contact->company->tel}}</a></li>
-                <li><span>Website: </span> <a href="http://{{$stage->contact->company->website}}" rel="nofollow">{{$stage->contact->company->website}}</a></li>
-                <li><span>Richting: </span> {{$stage->education->cohort->crebo->name}}</li>
-                <li><span>Leerweg: </span> {{$stage->education->leerweg}}</li>
+                <li><span>Locatie: </span>{{$stage->contact->company->fullAddress() }}</li>
+                <li><span>Telefoon: </span><a href="tel:{{$stage->contact->company->tel}}">{{$stage->contact->company->tel}}</a></li>
+                <li><span>Website: </span><a href="http://{{$stage->contact->company->website}}" rel="nofollow">{{$stage->contact->company->website}}</a></li>
+                <li><span>Richting: </span>{{$stage->education->cohort->crebo->name}}</li>
+                <li><span>Leerweg: </span>{{$stage->education->leerweg}}</li>
             </ul>
         </header>
     </article>
