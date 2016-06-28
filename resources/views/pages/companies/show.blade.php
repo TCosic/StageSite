40c2b10cf99f40c8f0e38f947cb80054510e9783
Tim Borowy <blastertimbo@gmail.com>
2016-06-28 09:05:18 +0200

    add create/edit buttons, fix login and various other fixes

--- resources/views/pages/companies/show.blade.php
+++ resources/views/pages/companies/show.blade.php
@@ -5,6 +5,8 @@
 @section('content')
     <section>
         <h2>Bedrijfs Details</h2>
+        <a href="{{route('bedrijf.edit', $company->id)}}" class="button green">Edit</a>
+        <a href="{{ route('bedrijf.destroy', $company->id) }}" data-token="{{ csrf_token() }}" class="delete button green" >Delete</a>
         <article>
             <header>
                 <h3>{{$company->name}}</h3>
