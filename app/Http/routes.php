232dc58c620e5c9610309369f27d50f5396d3021
Tarik Cosic <tarikcosic98@gmail.com>
2016-09-07 10:03:41 +0200

    hmmm

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -6,14 +6,14 @@
 Route::auth();
 
 
-        Route::resource('stage', 'InternshipController', ['only' => ['index', 'show'/*, 'create', 'edit'*/]]);
-        Route::resource('bedrijf', 'CompanyController', ['only' => ['index', 'show'/*, 'create', 'edit'*/]]);
+Route::resource('stage', 'InternshipController', ['only' => ['index', 'show'/*, 'create', 'edit'*/]]);
+Route::resource('bedrijf', 'CompanyController', ['only' => ['index', 'show'/*, 'create', 'edit'*/]]);
 
-        Route::group(['middleware' => ['auth']], function() {
-            Route::resource('stage', 'InternshipController', ['only' => ['create', 'edit']]);
-            Route::resource('bedrijf', 'CompanyController', ['only' => ['create', 'edit']]);
+Route::group(['middleware' => ['auth']], function() {
+    Route::resource('stage', 'InternshipController', ['only' => ['create', 'edit']]);
+    Route::resource('bedrijf', 'CompanyController', ['only' => ['create', 'edit']]);
 
-        });
+});
 
 Route::group(['namespace' => 'Web'], function(){
     Route::get('/', 'IndexController@index')->name('index');
@@ -29,11 +29,11 @@ Route::group(['namespace' => 'Web'], function(){
 
 });
 
-    /**
-     * Logged in Routes
-     */
-    Route::group(['middleware' => ['auth'], 'namespace' => 'Web', 'as' => 'login.'], function () {
-        Route::get('accounts', 'AccountController@index')->name('accounts.index');
+/**
+ * Logged in Routes
+ */
+Route::group(['middleware' => ['auth'], 'namespace' => 'Web', 'as' => 'login.'], function () {
+    Route::get('account', 'AccountController@index')->name('account.index');
 
     Route::group(['middleware' => ['login'], 'as' => 'login.'], function() {
         Route::get('admin', 'AdminController@index')->name('admin.index');
@@ -41,15 +41,13 @@ Route::group(['namespace' => 'Web'], function(){
 });
 
 
-    /**
-     * Api Routes
-     */
-    Route::group(['middleware' => ['api'], 'namespace' => 'Api'], function() {
-        Route::post('stage/search', 'InternshipController@search')->name('stage.search');
-        Route::post('stage/review', 'InternshipController@review')->name('stage.review');
-        Route::resource('stage', 'InternshipController', ['only' => ['store', 'update', 'destroy']]);
-        Route::resource('bedrijf', 'CompanyController', ['only' => ['store', 'update', 'destroy']]);
-    });
+/**
+ * Api Routes
+ */
+Route::group(['middleware' => ['api'], 'namespace' => 'Api'], function() {
+    Route::post('stage/search', 'InternshipController@search')->name('stage.search');
+    Route::resource('stage', 'InternshipController', ['only' => ['store', 'update', 'destroy']]);
+    Route::resource('bedrijf', 'CompanyController', ['only' => ['store', 'update', 'destroy']]);
 });
 
 
